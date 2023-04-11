86a7379cef045608b2a84d651411f2c8664bfbcf
reshad21 <miakala7@gmail.com>
2023-04-11 15:58:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-orange-700">
+        Hello world!
+      </h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  )
+}
+
+export default App
