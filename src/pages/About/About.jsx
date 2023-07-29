e5a646c0110178270308001fdf49b7270b2af347
reshad21 <miakala7@gmail.com>
2023-07-29 11:52:39 +0600

    work detailas speech add

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,104 @@
+import ProgressBar from "@ramonak/react-progress-bar";
+import CountUp from "react-countup";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "./About.css";
+
+const About = () => {
+    return (
+        <div className="p-5 relative about" id="about">
+            <div>
+                <img
+                    className="absolute left-48 top-64 "
+                    data-aos="zoom-in-down"
+                    src="https://i.ibb.co/D7B9JTn/bars12.png"
+                    alt=""
+                />
+            </div>
+            <div className="max-w-screen-xl mx-auto">
+                <div className="text-center p-36">
+                    <h2 className="text-6xl banner-heading-sub">About Me</h2>
+                </div>
+                <div className="md:flex justify-center items-center md:space-x-20 p-10">
+                    <div
+                        className="flex-1 text-center wrap-image relative"
+                        data-aos="zoom-in-left"
+                    >
+                        <img
+                            className="text-center inline rounded-2xl"
+                            src="https://i.ibb.co/SnrpzZw/passport-size-1-2.jpg"
+                            alt=""
+                        />
+
+                        <div className="flex items-center text-5xl justify-between bg-white p-5 rounded-2xl w-72 absolute top-2/3 right-10 shadow-xl">
+                            <p className=" banner-heading-sub text-5xl font-semibold">
+                                <CountUp start={0} end={50} duration={10} />+
+                            </p>
+                            <p className="font-semibold text-slate-400">
+                                Total <br /> Project
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex-1 sm:p-0 px-36 pt-10" data-aos="zoom-in-right">
+                        <Tabs>
+                            <TabList>
+                                <Tab>
+                                    <h3 className="text-3xl">About me</h3>
+                                </Tab>
+                                <Tab>
+                                    <h3 className="text-3xl">My Skills</h3>
+                                </Tab>
+                            </TabList>
+
+                            <TabPanel>
+                                <div className="mt-10">
+                                    <p className="banner-heading-sub text-4xl">Im a Designer</p>
+                                    <h3 className="text-6xl capitalize my-5">
+                                        I can design anything and <br /> develope you want
+                                    </h3>
+                                    <p className="text-3xl text-slate-500">
+                                        I am Sharmin Akhi, a highly skilled and passionate
+                                        Professional Web Designer with 4+ years of experience. I
+                                        specialize in PSD to HTML, Bootstrap, Tailwind CSS,
+                                        JavaScript, React, MongoDB, Node.js, and Firebase. I
+                                        prioritize effective communication, delivering high-quality
+                                        work, and providing ongoing support to clients. Lets
+                                        collaborate and bring your project to life!. <br /> Best
+                                        regards, <br /> Sharmin Akhi
+                                    </p>
+                                    <button className="btn-main mt-10">Hire Me</button>
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="mt-10">
+                                    <div>
+                                        <h3 className="text-3xl text-[#0078f3]">Html5</h3>
+                                        <ProgressBar bgColor="#ef00b7" height={15} completed={90} />
+                                    </div>
+                                    <div className="my-10">
+                                        <h3 className="text-3xl text-[#0078f3] ">Css3</h3>
+                                        <ProgressBar bgColor="#ef00b7" height={15} completed={90} />
+                                    </div>
+                                    <div className="mb-10">
+                                        <h3 className="text-3xl text-[#0078f3]">Javascript</h3>
+                                        <ProgressBar bgColor="#ef00b7" height={15} completed={80} />
+                                    </div>
+                                    <div>
+                                        <h3 className="text-3xl text-[#0078f3]">React</h3>
+                                        <ProgressBar bgColor="#ef00b7" height={15} completed={80} />
+                                    </div>
+                                    <div className="my-10">
+                                        <h3 className="text-3xl text-[#0078f3] ">Redux</h3>
+                                        <ProgressBar bgColor="#ef00b7" height={15} completed={85} />
+                                    </div>
+                                </div>
+                            </TabPanel>
+                        </Tabs>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/WorkDetails/WorkDetails.jsx
+++ src/pages/WorkDetails/WorkDetails.jsx
@@ -5,7 +5,7 @@ const WorkDetails = () => {
         <div className="max-w-screen-xl mx-auto sm:p-5 p-48" data-aos="zoom-in-up">
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 -mt-16 gap-36">
                 <div className="bg-white p-5 shadow-xl shadow-box rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                    <div className="md:flex items-center space-x-10 justify-center">
+                    <div className="md:flex items-center space-x-2 justify-center">
                         <img
                             className="inline"
                             src="https://i.ibb.co/mRVqGQf/1.png"
@@ -13,15 +13,14 @@ const WorkDetails = () => {
                         />
                         <div>
                             <h3 className="text-4xl mb-3">Pixel Perfect</h3>
-                            <p className="text-3xl text-slate-500">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                Soluta, tempora asperiores facilis.
+                            <p className="text-2xl text-slate-500">
+                                Pixel Perfect expert with a keen eye for detail, delivering flawless designs and exceptional user experiences. Elevate your projects with my precision and dedication.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="bg-white p-5 shadow-xl shadow-box rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                    <div className="md:flex items-center space-x-10 work-details">
+                    <div className="md:flex items-center space-x-2 work-details">
                         <img
                             className="inline"
                             src="https://i.ibb.co/84w10wQ/2.png"
@@ -29,15 +28,14 @@ const WorkDetails = () => {
                         />
                         <div>
                             <h3 className="text-4xl mb-3">high Quality</h3>
-                            <p className="text-3xl text-slate-500">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                Soluta, tempora asperiores.
+                            <p className="text-2xl text-slate-500">
+                                Experienced frontend web developer and skilled React specialist, crafting captivating user interfaces and delivering top-notch digital solutions. Let my expertise elevate your projects and create remarkable user experiences.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="bg-white p-5 shadow-xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                    <div className="md:flex items-center space-x-10">
+                    <div className="md:flex items-center space-x-2">
                         <img
                             className="inline"
                             src="https://i.ibb.co/vZbYX1w/3.png"
@@ -45,9 +43,8 @@ const WorkDetails = () => {
                         />
                         <div>
                             <h3 className="text-4xl mb-3">Awesome Idea</h3>
-                            <p className="text-3xl text-slate-500">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                Soluta, tempora asperiores facilis.
+                            <p className="text-2xl text-slate-500">
+                                Introducing a groundbreaking web app that seamlessly simplifies your life. With its intuitive interface and powerful features, it's the ultimate solution you've been waiting for. Embrace the future of productivity and experience a whole new level of convenience.
                             </p>
                         </div>
                     </div>
