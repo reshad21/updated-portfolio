782264b47fc46afdc7b407b362674d4fd6ecc79c
reshad21 <miakala7@gmail.com>
2023-07-31 12:36:06 +0600

    footer section add

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer section</h1>
+            <footer className="footer footer-center p-10 bg-base-300 text-base-content">
+                <div>
+                    <p className="text-3xl">
+                        Copyright © 2023 - All right reserved by{" "}
+                        <span className="banner-heading-sub">Rashed Uzzaman Reshad</span> Industries
+                        Ltd
+                    </p>
+                </div>
+            </footer>
         </div>
     );
 };
