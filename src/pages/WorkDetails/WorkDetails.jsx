47d39e8e6a5942cac885b24ff6a34a4430fedd7a
reshad21 <miakala7@gmail.com>
2023-11-25 11:06:45 +0600

    work details color change

--- src/pages/WorkDetails/WorkDetails.jsx
+++ src/pages/WorkDetails/WorkDetails.jsx
@@ -8,7 +8,7 @@ const WorkDetails = () => {
         <div className="max-w-screen-xl mx-auto p-36 md:p-48 lg:p-48" data-aos="zoom-in-up">
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 -mt-16 gap-20 md:gap-14 lg:gap-9">
 
-                <div className="p-5 nm-flat-red-500 work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <div className="p-5 shadow-lg work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                     <div className="flex items-center space-x-2 flex-col gap-6 pb-4">
                         <span><TbTargetArrow size={60} color='#F4E869' /></span>
                         <div className="text-center">
@@ -20,7 +20,7 @@ const WorkDetails = () => {
                     </div>
                 </div>
 
-                <div className="p-5 work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <div className="p-5 shadow-lg work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                     <div className="flex items-center space-x-2 flex-col gap-6 pb-4">
                         <span><FcIdea size={60} /></span>
                         <div className="text-center">
@@ -32,7 +32,7 @@ const WorkDetails = () => {
                     </div>
                 </div>
 
-                <div className="p-5 work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <div className="p-5 shadow-lg work_details rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                     <div className="flex items-center space-x-2 flex-col gap-6 pb-4">
                         <span><LuPaintbrush2 size={60} color='#F4E869' /></span>
                         <div className="text-center">
